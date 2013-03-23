929c0420766033b38520d0c334c14545573b6298
jerome-windows-de-merde <heissler.jerome@gmail.com>
2013-03-23 23:14:18 +0100

    adding depence

--- project/Build.scala
+++ project/Build.scala
@@ -12,15 +12,23 @@ object ApplicationBuild extends Build {
     javaCore,
     //javaJdbc,
     //javaEbean,
-    "jdom" % "jdom" % "1.1"
+    "jdom" % "jdom" % "1.1",
+	"com.github.jmkgreen.morphia" % "morphia" % "1.2.2",
+      ("com.github.jmkgreen.morphia" % "morphia-logging-slf4j" % "1.2.2" % "compile" notTransitive())
+        .exclude("org.slf4j", "slf4j-simple")
+        .exclude("org.slf4j", "slf4j-jdk14"),
+      ("com.github.jmkgreen.morphia" % "morphia-validation" % "1.2.2" % "compile" notTransitive())
+        .exclude("org.slf4j", "slf4j-simple")
+        .exclude("org.slf4j", "slf4j-jdk14")
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers ++= Seq(DefaultMavenRepository, Resolvers.morphiaRepository)      
+    resolvers ++= Seq(DefaultMavenRepository, Resolvers.morphiaRepository, Resolvers.typesafeRepository)      
   )
   
   object Resolvers {
     val morphiaRepository = "Morphia repository" at "http://morphia.googlecode.com/svn/mavenrepo/"
+	val typesafeRepository = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   }
 
 }
